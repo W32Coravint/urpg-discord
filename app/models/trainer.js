5c545e4a5cd224c8e59c6454dd16fd0ba75c672b
Ryan Munro <126519@uts.edu.au>
2018-07-13 11:28:22 +1000

    Updated start command and trainer model

--- app/models/trainer.js
+++ app/models/trainer.js
@@ -5,11 +5,14 @@ var trainerSchema = new Schema({
     discord_id: { type: String, required: true, unique: true },
     username: { type: String, required: true },
     cash: { type: Number, required: true, default: 10000 },
+    contestCredit: { type: Number, required: true, default: 5000 },
     joinDate: { type: Date, required: true, default: Date.now },
     battleRecord: { 
         wins: { type: Number, default: 0 }, 
         losses: { type: Number, default: 0 } 
-    }
+    },
+    starter: { type: String },
+    canChangeStarter: { type: Boolean, default: true }
 })
 
 module.exports = mongoose.model('Trainer', trainerSchema)
