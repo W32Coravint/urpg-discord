13333e5f589437c1c0c031506c151e8386108bbe
Ryan Munro <126519@uts.edu.au>
2018-07-13 11:28:22 +1000

    Updated start command and trainer model

--- app/commands/starter.js
+++ app/commands/starter.js
@@ -8,9 +8,21 @@ exports.run = (client, message, args) => {
         username: message.member.nickname || message.author.username
     })
 
-    trainer.save((err, trainer) => {
+    console.log(message.flags);
+    console.log(args);
+
+    trainer.save((err, saved) => {
         if(err.code == 11000) {
             message.channel.send("Sorry, you've already started playing URPG!")
+            logger.info(`New trainer request from ${trainer.username} rejected - already exists`, {key:"starter"})
+        }
+        else if(err) {
+            message.channel.send("An unknown error has occured - please notify an admin")
+            logger.error(error, {key:"starter"})
+        }
+        else {
+            message.channel.send(`Congratulations! New trainer ${trainer.username} registered.`)
+            logger.info(`New trainer ${trainer.username} (${trainer.discord_id}) registered.`, {key:"starter"})
         }
     })
 
@@ -22,7 +34,8 @@ exports.init = (client) => {
 
 exports.conf = {
     name: "starter",
-    enabled: true,
+    enabled: false,
+    aliases: ["start"]
 }
 
 exports.help = {
